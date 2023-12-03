05e9846323541408cec23c278ad482c4ae6616a3
21-jayashree <117674427+21-jayashree@users.noreply.github.com>
2023-12-03 17:46:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ def count_batteries_by_health(present_capacities):
     "healthy": 0,
     "exchange": 0,
     "failed": 0
-  }  '''name the dict for convinience'''
-  rated_cap=120 '''assume'''
+  }  #name the dict for convinience
+  rated_cap=120 #assume
 
   for present_cap in present_capacities:
-    soh=round((present_cap/rated_cap)*100) '''given formula'''
-    if soh>80.00:
+    soh=round((present_cap/rated_cap)*100)  #given formula
+    if soh>80:
       counts["healthy"]+=1
-    elif soh<=80.00 and soh>=62.00:
+    elif soh<=80 and soh>=62:
       counts["exchange"]+=1
     else:
       counts["failed"]+=1
@@ -26,14 +26,14 @@ def test_bucketing_by_health():
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
   print("Done counting :)")
-#making all efficient
+#making all efficient:upper boundry
   present_capacities = [120,120,120,120]
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 4)
   assert(counts["exchange"] == 0)
   assert(counts["failed"] == 0)
   print("Done counting :)")
-#giving empty list
+#giving empty list:null
   present_capacities = []
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 0)
@@ -41,12 +41,22 @@ def test_bucketing_by_health():
   assert(counts["failed"] == 0)
   print("Done counting :)")
 
-#giving float values
-  present_capacities = [113.5, 116.67, 80.7, 95.8, 92.90, 70.8]
+#giving float values:typecast
+  present_capacities = [113.4, 116.12, 80.00, 95.1, 92, 70.3]
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
   print("Done counting :)")
+
+#all minimum health:lower boundry
+  present_capacities = [0,0,0,0]
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 0)
+  assert(counts["exchange"] == 0)
+  assert(counts["failed"] == 4)
+  print("Done counting :)")
+
+
 if __name__ == '__main__':
   test_bucketing_by_health()
