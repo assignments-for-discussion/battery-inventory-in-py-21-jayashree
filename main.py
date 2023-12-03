cd5881f528cbb7a2a8d634437524fe5997ed24f4
21-jayashree <117674427+21-jayashree@users.noreply.github.com>
2023-12-03 17:34:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def count_batteries_by_health(present_capacities):
-  count_req= {  
+  counts= {  
     "healthy": 0,
     "exchange": 0,
     "failed": 0
@@ -8,14 +8,14 @@ def count_batteries_by_health(present_capacities):
   rated_cap=120 '''assume'''
 
   for present_cap in present_capacities:
-    soh=(present_cap/rated_cap)*100  '''given formula'''
-    if soh>80:
-      count_req["healthy"]=cout_req["healthy"]+1
-    elif soh<=80 and soh>=62:
-      count_req["exchange"]=cout_req["exchange"]+1
+    soh=round((present_cap/rated_cap)*100) '''given formula'''
+    if soh>80.00:
+      counts["healthy"]+=1
+    elif soh<=80.00 and soh>=62.00:
+      counts["exchange"]+=1
     else:
-      count_req["failed"]=cout_req["failed"]+1
-  return count_req
+      counts["failed"]+=1
+  return counts
 
 
 def test_bucketing_by_health():
@@ -26,7 +26,27 @@ def test_bucketing_by_health():
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
   print("Done counting :)")
+#making all efficient
+  present_capacities = [120,120,120,120]
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 4)
+  assert(counts["exchange"] == 0)
+  assert(counts["failed"] == 0)
+  print("Done counting :)")
+#giving empty list
+  present_capacities = []
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 0)
+  assert(counts["exchange"] == 0)
+  assert(counts["failed"] == 0)
+  print("Done counting :)")
 
-
+#giving float values
+  present_capacities = [113.5, 116.67, 80.7, 95.8, 92.90, 70.8]
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 2)
+  assert(counts["exchange"] == 3)
+  assert(counts["failed"] == 1)
+  print("Done counting :)")
 if __name__ == '__main__':
   test_bucketing_by_health()
