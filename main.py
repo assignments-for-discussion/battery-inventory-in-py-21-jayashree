4b9556f0b10be6bb1cf3dd871c75e69e544f71ef
21-jayashree <117674427+21-jayashree@users.noreply.github.com>
2023-12-03 17:18:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
+  count_req= {  
     "healthy": 0,
     "exchange": 0,
     "failed": 0
-  }
+  }  '''name the dict for convinience'''
+  rated_cap=120 '''assume'''
+
+  for present_cap in present_capacities:
+    soh=(present_cap/rated_cap)*100  '''given formula'''
+    if soh>80:
+      count_req["healthy"]=cout_req["healthy"]+1
+    elif soh<=80 and soh>=62:
+      count_req["exchange"]=cout_req["exchange"]+1
+    else:
+      count_req["failed"]=cout_req["failed"]+1
+  return count_req
 
 
 def test_bucketing_by_health():
